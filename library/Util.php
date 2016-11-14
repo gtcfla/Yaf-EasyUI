99f57f880609ee6978d73c6d1cf391ccb2229045
gtcfla <gtcfla@gmail.com>
2016-11-14 18:12:34 +0800

    工具类完善

--- library/Util.php
+++ library/Util.php
@@ -2,39 +2,37 @@
 
 class Util
 {
-	public static function directory_map($dir)
+	/**
+	 * 取得目录下及所有子孙目录的文件路径，并且以$dir参数为根目录名
+	 *
+	 * @author liaofuqian
+	 * @param String $dir 路径名
+	 * @param String $filter 正则表达式，过滤掉文件名不匹配该表达式的文件
+	 * @return array
+	 */
+	public static function directory_map($dir, $filter = null)
 	{
-		print_r(scandir($dir));exit;
-		if ($fp = @opendir($source_dir))
-		{
-			$filedata	= array();
-			$new_depth	= $directory_depth - 1;
-			$source_dir	= rtrim($source_dir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
-
-			while (FALSE !== ($file = readdir($fp)))
-			{
-				// Remove '.', '..', and hidden files [optional]
-				if ($file === '.' OR $file === '..' OR ($hidden === FALSE && $file[0] === '.'))
-				{
-					continue;
-				}
-
-				is_dir($source_dir.$file) && $file .= DIRECTORY_SEPARATOR;
-
-				if (($directory_depth < 1 OR $new_depth > 0) && is_dir($source_dir.$file))
-				{
-					$filedata[$file] = directory_map($source_dir.$file, $new_depth, $hidden);
-				}
-				else
-				{
-					$filedata[] = $file;
-				}
+		
+		$dirpath = realpath($dir);
+		
+		$arr_file = [];
+		$filenames = scandir($dir, 1);
+		
+		for ($i = 0, $count = count($filenames); $i < $count; $i++) {
+			
+			$filename = $filenames[$i];
+		
+			if ($filename == '.' || $filename == '..' || $filename == '.svn' || ( !empty($filter) && preg_match($filter, $filename))) {
+				continue;
 			}
-
-			closedir($fp);
-			return $filedata;
+		
+			$file = $dirpath . DIRECTORY_SEPARATOR . $filename;
+			if (is_dir($file)) {
+				$arr_file = array_merge( $arr_file, self::directory_map($file));
+			} else {
+				$arr_file[] = $file;
+			}
 		}
-
-		return FALSE;
+		return $arr_file;
 	}
 }
